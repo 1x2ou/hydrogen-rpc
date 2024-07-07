399cd6e9f0eec734337ec58a3d95810e11175414
zlx <505925146@qq.com>
2024-07-07 19:19:45 +0800

    feat:公共模块开发

--- example-common/src/main/java/com/hydrogen/example/common/service/UserService.java
+++ example-common/src/main/java/com/hydrogen/example/common/service/UserService.java
@@ -1,4 +1,12 @@
 package com.hydrogen.example.common.service;
 
+import com.hydrogen.example.common.model.User;
+
 public interface UserService {
+    /**
+     * 获取用户
+     * @param user
+     * @return
+     */
+    User getUser(User user);
 }

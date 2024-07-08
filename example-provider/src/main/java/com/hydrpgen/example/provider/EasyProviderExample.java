b31c6ab75d27a4831a283c6b42ab7accb34e6899
zlx <505925146@qq.com>
2024-07-08 17:16:07 +0800

    feat:本地注册服务器

--- example-provider/src/main/java/com/hydrpgen/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/hydrpgen/example/provider/EasyProviderExample.java
@@ -1,7 +1,16 @@
 package com.hydrpgen.example.provider;
 
+import com.hydrogen.example.common.service.UserService;
+import com.hydrogen.hydrogenrpc.HttpServer;
+import com.hydrogen.hydrogenrpc.VertxHttpServer;
+import com.hydrpgen.example.registry.LocalRegistry;
+
 public class EasyProviderExample {
     public static void main(String[] args) {
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
         //提供服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }

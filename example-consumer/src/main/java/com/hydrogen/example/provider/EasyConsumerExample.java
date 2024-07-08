a0c67e8a92232a75fe617d946aeadcbadd446fd4
zlx <505925146@qq.com>
2024-07-08 19:21:47 +0800

    feat:动态代理

--- example-consumer/src/main/java/com/hydrogen/example/provider/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/hydrogen/example/provider/EasyConsumerExample.java
@@ -2,19 +2,21 @@ package com.hydrogen.example.provider;
 
 import com.hydrogen.example.common.model.User;
 import com.hydrogen.example.common.service.UserService;
+import com.hydrogen.hydrogenrpc.proxy.ServiceProxyFactory;
 
 public class EasyConsumerExample {
     public static void main(String[] args) {
-        //静态代理
-        UserService userService = new UserServiceProxy();
-
+//        //静态代理
+//        UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("lingxiao");
         //调用
         User newUser = userService.getUser(user);
-        if(newUser != null){
+        if (newUser != null) {
             System.out.println(newUser.getName());
-        }else{
+        } else {
             System.out.println("user == null");
         }
     }
